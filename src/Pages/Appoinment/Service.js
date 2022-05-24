1d7cff112734a872edb744b2677bb18e39a13f23
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2022-05-25 03:04:01 +0600

    doctors service modal added and connected with mongodb

--- src/Pages/Appoinment/Service.js
+++ src/Pages/Appoinment/Service.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const Service = ({ service }) => {
+const Service = ({ service, setTreatment }) => {
     const { name, slots } = service
     return (
         <div class="card bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title">Card title!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="card-body text-center">
+                <h2 class="card-title text-secondary mx-auto pb-3">{name}</h2>
+                <p>{slots.length ? <span>{slots[0]}</span> :
+                    <span className='text-red-400'>Not Available Now</span>
+                }</p>
+
+                <p>{slots.length} {slots.length > 1 ? 'Slots' : 'Slot'} Available</p>
+                <div class="card-actions justify-center py-5">
+                    <label onClick={() => setTreatment(service)}
+                        disabled={slots.length === 0}
+                        for="booking-modal" class="btn btn-primary uppercase bold text-white">Book Appoinment</label>
                 </div>
             </div>
         </div>
